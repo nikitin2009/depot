2cf98743470b46b68a6000cc6753525b88fbca41
Alex <nikitin2009@gmail.com>
2019-05-15 15:27:25 +0300

    Add emptying cart with Ajax

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -58,6 +58,7 @@ class CartsController < ApplicationController
     session[:cart_id] = nil
     respond_to do |format|
       format.html { redirect_to store_index_url, notice: 'Your cart is currently empty.' }
+      format.js
       format.json { head :no_content }
     end
   end
