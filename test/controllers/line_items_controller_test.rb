71f4e5fc1d17b04de31e3ba3bf86c543228c66fd
Alex <nikitin2009@gmail.com>
2019-05-16 12:47:26 +0300

    Implement checkout

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -46,7 +46,7 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
 
   test "should update line_item" do
     patch line_item_url(@line_item), params: { line_item: { product_id: @line_item.product_id } }
-    assert_redirected_to line_item_url(@line_item)
+    assert_redirected_to store_index_url
   end
 
   test "should destroy line_item" do
