82adbb9e5824e045b0962ac10f645668fc909dcf
Alex <nikitin2009@gmail.com>
2019-05-17 16:17:15 +0300

    Implement User authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
+  get 'admin', to: 'admin#index'
+
+  controller :sessions do
+    get  'login' => :new
+    post 'login' => :create
+    delete  'logout' => :destroy
+  end
+
   resources :users
   resources :orders
   resources :line_items
   resources :carts
   root 'store#index', as: 'store_index'
+
   resources :products do
     get :who_bought, on: :member
   end
