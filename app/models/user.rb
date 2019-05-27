17aea43d77b299bc32268296e5df16069778c1d9
Alex <nikitin2009@gmail.com>
2019-05-27 12:11:01 +0300

    Adding a sidebar, more administration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ApplicationRecord
+  after_destroy :ensure_an_admin_remains
+
+  class Error < StandardError
+  end
+
   validates :name, presence: true, uniqueness: true
   has_secure_password
+
+  private
+    def ensure_an_admin_remains
+      if User.count.zero?
+        raise Error.new "Can't delete last user"
+      end
+    end
 end
